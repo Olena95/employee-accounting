b026179792a47152ea1260317ba9869fcca147be
Olena Hrynykha <olenagrynyha@gmail.com>
2022-04-09 18:34:50 +0300

    add components

---
+++ my-app/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Employee accounting in Steelton company</h1>
+      <h2>Total number of employees: </h2>
+      <h4>Will receive a cash prize:</h4>
+    </div>
+  );
+};
+
+export default AppInfo;
